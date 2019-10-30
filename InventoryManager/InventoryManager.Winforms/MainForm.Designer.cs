9700c7e8e59f30ae0f3e14cd0afbcfe8a0f3936b
KyleLambdin78 <43045070+KyleLambdin78@users.noreply.github.com>
2019-10-30 15:18:02 -0400

    26:50

---
+++ InventoryManager/InventoryManager.Winforms/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace InventoryManager.Winforms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label fileLabel;
+            this.selectFileButton = new System.Windows.Forms.Button();
+            this.filenameTextBox = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.worldViewModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            fileLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.worldViewModelBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileLabel
+            // 
+            fileLabel.AutoSize = true;
+            fileLabel.Location = new System.Drawing.Point(12, 9);
+            fileLabel.Name = "fileLabel";
+            fileLabel.Size = new System.Drawing.Size(23, 13);
+            fileLabel.TabIndex = 0;
+            fileLabel.Text = "File";
+            // 
+            // selectFileButton
+            // 
+            this.selectFileButton.Location = new System.Drawing.Point(548, 6);
+            this.selectFileButton.Name = "selectFileButton";
+            this.selectFileButton.Size = new System.Drawing.Size(29, 23);
+            this.selectFileButton.TabIndex = 1;
+            this.selectFileButton.Text = "...";
+            this.selectFileButton.UseVisualStyleBackColor = true;
+            this.selectFileButton.Click += new System.EventHandler(this.SelectFileButton_Click);
+            // 
+            // filenameTextBox
+            // 
+            this.filenameTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.worldViewModelBindingSource, "Filename", true));
+            this.filenameTextBox.Location = new System.Drawing.Point(41, 6);
+            this.filenameTextBox.Name = "filenameTextBox";
+            this.filenameTextBox.ReadOnly = true;
+            this.filenameTextBox.Size = new System.Drawing.Size(501, 20);
+            this.filenameTextBox.TabIndex = 2;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "World Files (*json)|*.json";
+            // 
+            // worldViewModelBindingSource
+            // 
+            this.worldViewModelBindingSource.DataSource = typeof(InventoryManager.Winforms.ViewModels.WorldViewModel);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 434);
+            this.Controls.Add(this.filenameTextBox);
+            this.Controls.Add(this.selectFileButton);
+            this.Controls.Add(fileLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "InventoryManager";
+            ((System.ComponentModel.ISupportInitialize)(this.worldViewModelBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectFileButton;
+        private System.Windows.Forms.TextBox filenameTextBox;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.BindingSource worldViewModelBindingSource;
+    }
+}
+
---
+++ InventoryManager/InventoryManager.Winforms/ViewModels/WorldViewModel.cs
@@ -0,0 +1,11 @@
+﻿using InventoryManager.Data;
+
+namespace InventoryManager.Winforms.ViewModels
+{
+   public class WorldViewModel
+    {
+        public string Filename { get; set; }
+
+        public World World { get; set; }
+    }
+}
