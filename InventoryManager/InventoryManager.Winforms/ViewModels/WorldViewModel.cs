079c7164736cdfe34d95be92c999803a6354dd64
KyleLambdin78 <43045070+KyleLambdin78@users.noreply.github.com>
2019-11-05 19:14:10 -0500

    Dakarai will be getting a lot of skittles for this if this works

--- InventoryManager/InventoryManager.Winforms/ViewModels/WorldViewModel.cs
+++ InventoryManager/InventoryManager.Winforms/ViewModels/WorldViewModel.cs
@@ -1,11 +1,19 @@
 ﻿using InventoryManager.Data;
+using System.ComponentModel;
+
 
 namespace InventoryManager.Winforms.ViewModels
 {
-   public class WorldViewModel
+    public class WorldViewModel: INotifyPropertyChanged
     {
+ 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Filename { get; set; }
 
         public World World { get; set; }
+ 
+
     }
 }
